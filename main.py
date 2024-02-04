c8217858ac5bcf3f297026ecf77dbea5f1cded4a
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-04 13:38:49 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from typing import Dict
+
+
+def format_date(date: str) -> str:
+    date_object = datetime.strptime(date, "%a %b %d %Y %H:%M:%S GMT%z")
+    return date_object.strftime("%Y-%m-%d")
+
+
+def get_articles() -> Dict[str, str]:
+    article_list = []
+    url = "https://www.freecodecamp.org/news/"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+
+    articles = doc.find_all(class_="post-card")
+    for article in articles:
+        obj = {}
+        obj["title"] = article.find("h2").get_text().strip()
+        href = article.find("a").get("href")
+        obj["link"] = f"{url[:-6:]}{href}"
+        author_items = article.find("footer")
+        obj["author"] = author_items.li.span.a.get_text().strip()
+        date = author_items.li.span.time.get("datetime").split(" (")[0]
+        obj["date"] = format_date(date)
+        obj["category"] = "freeCodeCamp"
+        article_list.append(obj)
+    return article_list
+
+
+fcc = get_articles()
+print(fcc[0])
