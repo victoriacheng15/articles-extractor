05abf7d19fbbe7ec4e60a83da2ec671af870537d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-02-05 11:22:17 -0700

    extractors sheet (#4)
    * chore: use tuple instead snice the data doesnt change
    
    * refactor: add check_existin_header and get_all_title functions
    
    * chore: declare variables inside the main function, import functions for header and get all title

--- utils/extractors.py
+++ utils/extractors.py
@@ -3,16 +3,22 @@ from utils.format_date import format_date
 
 
 def extract_fcc_articles(article):
-    obj = {}
-    obj["title"] = article.find("h2").get_text().strip()
+    title = article.find("h2").get_text().strip()
     href = article.find("a").get("href")
-    obj["link"] = f"https://www.freecodecamp.org{href}"
+    link = f"https://www.freecodecamp.org{href}"
     author_items = article.find("footer")
-    obj["author"] = author_items.li.span.a.get_text().strip()
+    author = author_items.li.span.a.get_text().strip()
     date = author_items.li.span.time.get("datetime").split(" (")[0]
-    obj["date"] = format_date(date)
-    obj["category"] = "freeCodeCamp"
-    return obj
+    date = format_date(date)
+    return date, title, author, link, "freeCodeCamp"
+
+def extract_substack_articles(article):
+    all_element = article.find_all("a")
+    title = all_element[0].get_text()
+    link = all_element[0].get("href")
+    author = all_element[-1].get_text() if len(all_element) == 3 else "Cannot find author name"
+    date = article.find("time").get("datetime").split("T")[0]
+    return date, title, author, link, "substack"
 
 
 def get_articles(url, class_name, extract_func, all_articles):
