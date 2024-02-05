5fcc175e4a3a09d05d577f48656c15c7b53dce6c
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-05 08:13:00 -0700

    refactor: use setup-google-sheet func for the connection

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-import os
-import gspread
-from google.oauth2.service_account import Credentials
-from dotenv import load_dotenv
 from utils.extractors import extract_fcc_articles
 from utils.extractors import get_articles
+from config.setup_google_sheet import setup_google_sheet
 
-load_dotenv()
-SHEET_ID = os.environ.get("SHEET_ID")
-
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
-client = gspread.authorize(creds)
-sheet = client.open_by_key(SHEET_ID)
-worksheet = sheet.sheet1
 
 header = ["date", "title", "author", "link", "category"]
-existing_header = worksheet.row_values(1)
+all_articles = []
 
+worksheet = setup_google_sheet()
+
+existing_header = worksheet.row_values(1)
 if existing_header != header:
     worksheet.append_row(header)
-
-all_articles = []
 
 
 def main():
@@ -30,7 +20,6 @@ def main():
         extract_fcc_articles,
         all_articles,
     )
-    print(all_articles)
     for article in all_articles:
         existing_article = worksheet.find(article["title"], in_column=2)
         if not existing_article:
