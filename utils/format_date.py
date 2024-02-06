c6bcab8046652e4bf0deadc288b1057d584f8a1d
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-06 08:54:22 -0700

    feat: add current_time function to get current time for logging

--- utils/format_date.py
+++ utils/format_date.py
@@ -4,3 +4,7 @@ from datetime import datetime
 def format_date(date: str) -> str:
     date_object = datetime.strptime(date, "%a %b %d %Y %H:%M:%S GMT%z")
     return date_object.strftime("%Y-%m-%d")
+
+
+def current_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f').split(".")[0]
