d82dee969f06f49956ef13412fb45a7159a8351a
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-04 07:07:57 -0700

    chore: remove check for existing header

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.sheet import main_sheet, check_existing_header, send_articles_sheet
+from utils.sheet import main_sheet, send_articles_sheet
 from utils.extractors import extract_fcc_articles, extract_substack_articles
 from utils.extractors import get_articles
 from utils.format_date import current_time
@@ -6,8 +6,6 @@ from data.providers import freecodecamp, substack
 
 
 def main(time):
-    check_existing_header()
-
     fcc_url = freecodecamp["url"]
     fcc_class = freecodecamp["class"]
     for article_info in get_articles(fcc_url, fcc_class, extract_fcc_articles):
