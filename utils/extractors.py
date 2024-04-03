a0c26f2b16baeef28cae7ffb46f97aa3f38cc750
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-03 12:52:15 -0600

    chore: add extract ztm function

--- utils/extractors.py
+++ utils/extractors.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 from utils.get_page import get_page
 from utils.format_date import format_date
 from utils.sheet import existing_titles
@@ -38,6 +40,17 @@ def extract_github_articles(article):
     )
     date = article.find("time").get("datetime")
     return date, title, authors, link, "github"
+
+def extract_ztm_articles(article):
+    pattern = re.compile(r'(\d+)(st|nd|rd|th)')
+    title = article.find("a").get_text()
+    href = article.find(class_="blog-archive-cardstyles__ArticleTitle-sc-1aquqyk-4 hTIlqp").get("href")
+    link = f"https://zerotomastery.io/{href}"
+    date = article.find("div",class_="post-preview-cardstyles__ArticleMetaData-sc-1wv8f3p-11").get_text().split(" ·")[0]
+    date_string = pattern.sub(r'\1', date)
+    date = datetime.strptime(date_string, "%B %d, %Y").strftime("%Y-%m-%d")
+    author = article.find(class_="blog-archive-cardstyles__ArticleAuthor-sc-1aquqyk-8 eLuqJA").get_text()
+    return date, title, author, link, "ztm"
 
 
 def get_articles(elements, extract_func):
