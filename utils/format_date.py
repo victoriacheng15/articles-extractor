4e85eeb27730d16b61c85f5c49b5d2b1e7c9f723
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-17 11:56:55 -0600

    chore: add clean and convert date function

--- utils/format_date.py
+++ utils/format_date.py
@@ -6,6 +6,15 @@ def format_date(date: str) -> str:
     return date_object.strftime("%Y-%m-%d")
 
 
+def clean_and_convert_date(date_str):
+    if date_str[0].isdigit():
+        date_obj = datetime.strptime(date_str[:10], "%Y-%m-%d")
+    else:
+        date_obj = datetime.strptime(date_str[4:16].strip(), "%b %d %Y")
+
+    return date_obj.strftime("%Y-%m-%d")
+
+
 def current_time():
     date = datetime.now().date()
     time = datetime.now().time().strftime("%H:%M")
