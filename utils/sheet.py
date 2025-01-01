77797a2638d6dd9194b7cdd6feae23591f3edc14
Victoria Cheng <victoria.cheng15@gmail.com>
2025-01-01 13:36:40 -0700

    add providers sheet (#39)
    * feat: get data from providers worksheet
    
    * feat: loop through provider list
    
    * chore: format files

--- utils/sheet.py
+++ utils/sheet.py
@@ -5,34 +5,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SHEET_ID = os.environ.get("SHEET_ID")
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 
 
 def get_creds():
     return os.path.join(os.path.dirname(__file__), "..", "credentials.json")
 
 
-def setup_google_sheet():
+def get_sheet(sheet_name):
     creds = get_creds()
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
     creds = Credentials.from_service_account_file(creds, scopes=scopes)
     client = gspread.authorize(creds)
     sheet = client.open_by_key(SHEET_ID)
-    return sheet.sheet1
+    return sheet.worksheet(sheet_name)
 
 
-main_sheet = setup_google_sheet()
-
-
-def check_existing_header():
-    header = ["date", "title", "author", "link", "category", "read?"]
-    existing_header = main_sheet.row_values(1)
-    if existing_header != header:
-        main_sheet.append_row(header)
+articles_sheet = get_sheet("articles")
+providers_sheet = get_sheet("providers")
 
 
 def get_all_titles():
-    data = main_sheet.get_all_values()[1:]
-    return tuple(row[1] for row in data)
+    all_titles = articles_sheet.get_all_values()[1:]
+    return tuple(row[1] for row in all_titles)
 
 
 existing_titles = get_all_titles()
@@ -43,4 +37,8 @@ def send_articles_sheet(article_info):
     title = article_info[1]
     author = article_info[2]
     print(f"===> adding {title} by {author} at {date}!")
-    main_sheet.append_row(list(article_info))
+    articles_sheet.append_row(list(article_info))
+
+
+def get_all_providers():
+    return providers_sheet.get_all_records()
