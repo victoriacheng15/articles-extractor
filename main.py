6090bed1cd8bc7e3e47d3be5246f17c91335a80e
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-02 11:09:17 -0700

    refactor: use dict to get properties instead of using if else

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
+import re
 from utils.sheet import articles_sheet, get_all_providers, send_articles_sheet
 from utils.get_page import get_page
-from utils.extractors import get_articles
-from utils.extractors import (
-    extract_fcc_articles,
-    extract_substack_articles,
-    extract_github_articles,
-)
+from utils.extractors import provider_dict, get_articles
 from utils.format_date import current_time
-import re
 
 
 def main(time):
@@ -18,20 +13,18 @@ def main(time):
         provider_url = provider["url"]
         provider_element = provider["element"]
 
-        if provider_name == "freecodecamp":
-            elements = get_page(provider_url).find_all(provider_element)
-            for article_info in get_articles(elements, extract_fcc_articles):
+        provider_handlers = provider_dict(provider_element)
+        handler = provider_handlers.get(provider_name)
+
+        if handler:
+            element = handler["element"]()
+            extractor = handler["extractor"]
+
+            elements = get_page(provider_url).find_all(element)
+            for article_info in get_articles(elements, extractor):
                 send_articles_sheet(article_info)
-        elif provider_name == "substack":
-            elements = get_page(provider_url).find_all(
-                class_=re.compile(provider_element)
-            )
-            for article_info in get_articles(elements, extract_substack_articles):
-                send_articles_sheet(article_info)
-        elif provider_name == "github":
-            elements = get_page(provider_url).find_all(provider_element)
-            for article_info in get_articles(elements, extract_github_articles):
-                send_articles_sheet(article_info)
+        else:
+            print(f"Unknown provider: {provider_name}")
 
     articles_sheet.sort((1, "des"))
     articles_sheet.update_cell(1, 7, f"Updated at\n{time}")
