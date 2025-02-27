b655b0e2d688e7d7d30398800a00428b015c5312
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-27 19:25:06 +0000

    chore: add docs for each function

--- utils/format_date.py
+++ utils/format_date.py
@@ -1,12 +1,30 @@
 from datetime import datetime
 
 
-def format_date(date: str) -> str:
+def format_date(date):
+    """
+    Converts a date string from the format "%a %b %d %Y %H:%M:%S GMT%z" to "%Y-%m-%d".
+
+    Args:
+        date (str): The date string to format.
+
+    Returns:
+        str: The formatted date string in the format "%Y-%m-%d".
+    """
     date_object = datetime.strptime(date, "%a %b %d %Y %H:%M:%S GMT%z")
     return date_object.strftime("%Y-%m-%d")
 
 
 def clean_and_convert_date(date_str):
+    """
+    Cleans and converts a date string to the format "%Y-%m-%d".
+
+    Args:
+        date_str (str): The date string to clean and convert.
+
+    Returns:
+        str: The cleaned and converted date string in the format "%Y-%m-%d".
+    """
     if date_str[0].isdigit():
         date_obj = datetime.strptime(date_str[:10], "%Y-%m-%d")
     else:
@@ -16,6 +34,12 @@ def clean_and_convert_date(date_str):
 
 
 def current_time():
+    """
+    Retrieves the current date and time.
+
+    Returns:
+        tuple: A tuple containing the current date and time in the format (date, time).
+    """
     date = datetime.now().date()
     time = datetime.now().time().strftime("%H:%M")
-    return date, time
+    return (date, time)
